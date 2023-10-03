361a82811de3812be85d2f20cf0bf6ae0e0033b9
pteacher <subfear@mail.ru>
2023-10-03 16:24:49 +0600

    voice_gen mrartur0074githubio64fdc95c69c9f.yaml

--- sript.py
+++ sript.py
@@ -5,9 +5,10 @@ import json
 import yaml
 import git
 
-
 URL = "http://185.68.22.253/download/mrartur0074githubio64fdc95c69c9f.yaml"
 file_name = os.path.basename(urlparse(URL).path)
+
+
 # json_dir = r'D:\projects\venv\old_conv'
 
 
@@ -42,3 +43,6 @@ try:
     origin.push()
 except Exception as e:
     print('An error occurred:', str(e))
+
+if __name__ == '__main__':
+    pass
